d4c4b946a9f460aa44513043d688b086e25e1ee4
MihaiDavid05 <davidmihai98@yahoo.com>
2023-05-03 01:30:01 +0200

    Fix coding style

--- docs/res/slider.js
+++ docs/res/slider.js
@@ -1,56 +1,53 @@
 var margin = { top: 50, right: 50, bottom: 0, left: 50 },
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var formatTickDate = d3.timeFormat("%A %e");
-var formatTooltipDate = d3.timeFormat("%A %e - %H:%M");
+var formatTickDate = d3.timeFormat("%Y");
+var formatTooltipDate = d3.timeFormat("%Y - %B");
 
-var startDate = Date.now();
-var endDate = new Date();
+var startDate = new Date(2018, 0, 1);
+var endDate = new Date(2022, 11, 31);
 
 Date.prototype.addDays = function (days) {
-  var date = new Date(this.valueOf());
-  date.setDate(date.getDate() + days);
-  return date;
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
 };
 
 var moving = false;
 var targetValue = width;
 var timer;
 
-endDate = endDate.addDays(10);
-
-var tickVals = [0,1,2,3,4,5,6,7,8,9,10].map(iter => new Date().setHours(24 * iter,0,0,0));
+var tickVals = [0, 1, 2, 3, 4].map(iter => new Date(2018 + iter, 0, 1));
 
 var playButton = d3.select("#play-button");
 
 var sliderTime = d3
-  .sliderBottom()
-  .min(startDate)
-  .max(endDate)
-  .step(1000 * 60 * 60 * 3)
-  .width(width + margin.left + margin.right - 20)
-  .tickFormat(formatTickDate)
-  .tickValues(tickVals)
-.fill('#2196f3')
-.displayFormat(formatTooltipDate)
-  .default(Date.now())
-  .handle(d3.symbol().type(d3.symbolCircle).size(200)())
-  .on("drag", function (val) {
-    resetTimer();
-  }).on("onchange", function (val) { 
+    .sliderBottom()
+    .min(startDate)
+    .max(endDate)
+    .step(1000 * 60 * 60 * 24 * 31)
+    .width(width + margin.left + margin.right - 20)
+    .tickFormat(formatTickDate)
+    .tickValues(tickVals)
+    .fill('#2196f3')
+    .displayFormat(formatTooltipDate)
+    .default(startDate)
+    .handle(d3.symbol().type(d3.symbolCircle).size(200)())
+    .on("drag", function (val) {
+        resetTimer();
+    }).on("onchange", function (val) {
         d3.select(".tick text").attr("opacity", "1");
         d3.select("p#value-time").text(formatTooltipDate(val))
-});
-//nell'onchange aggiorna i layers
+    });
 
 var gTime = d3
-  .select("div#slider-time")
-  .append("svg")
-  .attr("width", 1000)
-  .attr("height", 100)
-  .append("g")
-  .attr("transform", "translate(30,30)");
+    .select("div#slider-time")
+    .append("svg")
+    .attr("width", 1000)
+    .attr("height", 100)
+    .append("g")
+    .attr("transform", "translate(40,30)");
 
 gTime.call(sliderTime);
 
@@ -60,29 +57,28 @@ d3.selectAll(".tick text").style("text-anchor", "start");
 document.querySelector(".parameter-value path").removeAttribute("tabindex");
 
 playButton.on("click", function () {
-  var button = d3.select(this);
-  if (button.text() == "Pause") {
-   resetTimer();
-  } else {
-    moving = true;
-    timer = setInterval(update, 1000);
-    button.text("Pause");
-  }
+    var button = d3.select(this);
+    if (button.text() == "Pause") {
+        resetTimer();
+    } else {
+        moving = true;
+        timer = setInterval(update, 100);
+        button.text("Pause");
+    }
 });
 
-
 function update() {
-  var offset = sliderTime.value().valueOf() + 10800000;
-  sliderTime.value(offset);
-  //pause, uncomment to restart
-  if(offset >= endDate.valueOf()) {
-     resetTimer();
-   // sliderTime.value(startDate.valueOf());
-  }
+    var offset = sliderTime.value().valueOf() + (1000 * 60 * 60 * 24 * 31);
+    sliderTime.value(offset);
+    //pause, uncomment to restart
+    if (offset >= endDate.valueOf()) {
+        // resetTimer();
+        sliderTime.value(startDate.valueOf());
+    }
 }
 
 function resetTimer() {
-  moving = false;
+    moving = false;
     clearInterval(timer);
-   playButton.text("Play");
+    playButton.text("Play");
 }
