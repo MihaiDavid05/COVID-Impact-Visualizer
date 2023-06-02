ab684d528ea08ff952793cd73ed2c12eeb29f16b
Vuk Vukovic <vukovic.dev@gmail.com>
2023-06-02 03:08:18 +0200

    Timeline added. Counters improved.

--- docs/res/counter.js
+++ docs/res/counter.js
@@ -1,19 +1,37 @@
-$(function() {
-    function count($this){
-        var current = parseInt($this.html().replaceAll(",",""), 10);
-        const step = Math.floor($this.data('count') / 150)
-        current = current + step;
-        $this.html(current.toLocaleString("en-US"));
-        if(current > $this.data('count')){
-            $this.html($this.data('count').toLocaleString("en-US"));
-        } else {
-            setTimeout(function(){count($this)}, 5);
-        }
-    }        
-  $(".numbers-highlight-beginning").each(function() {
-    $(this).css('visibility', 'visible');
-    $(this).data('count', parseInt($(this).html(), 10));
-    $(this).html('0');
-    count($(this));
+function countStep(counter, final_num, current, step) {
+  current += step;
+  if (current > final_num)
+    current = final_num;
+  else {
+    setTimeout(function(){countStep(counter, final_num, current, step)}, 20);
+  }
+  counter.html(current.toLocaleString("en-US"));
+}
+
+$(function () {
+  $(".counter").each(function() {
+    let counter = $(this);
+    let start = parseInt(counter.data("start"));
+    counter.html(start);
+  });
+});
+
+$(window).scroll(function() {
+  let scroll = $(this);
+  $(".counter").each(function() {
+    let counter = $(this);
+    let hT = counter.offset().top,
+      hH = counter.outerHeight(),
+      wH = $(window).height(),
+      wS = scroll.scrollTop();
+    if (wS > (hT+hH-wH)) {
+      if (!counter.data("done")) {
+        counter.data("done", 1);
+        let final_num = parseInt(counter.data("final"));
+        let start = parseInt(counter.data("start"));
+        let step = parseInt(counter.data("step"));
+        setTimeout(function(){countStep(counter, final_num, start, step)}, 20);
+      }
+    }
   });
 });
